a9690fe0860d2bcff54deabef104148b31d18887
Dmitry <krio.alfa@gmail.com>
2022-10-03 16:47:29 +0300

    lection added

---
+++ 1/Homework/Dmitry_L1.py
@@ -0,0 +1,47 @@
+# Homework 1.0
+print('Usus est optimus magister') # Printing to console
+# Testing errors
+print 'Hello'
+print('Hello'
+print('Hello)
+
+# Homework 1.1
+print('Programing', 'Essantials', sep='***', end='...')
+print('Python')
+
+# Homework 1.2
+print("""
+    *
+   * *
+  *   *
+***   ***
+  *   *
+  *   *
+  *****
+""")
+print('    ','   * ','  *   ','***   **','  *   ','  *   *',sep='*\n',end='  \n   *****\n')
+
+# Homework 1.3
+# print('"I`m"'+'\n""learning','"Python',sep='""\n""',end='"""\n')
+
+# Homework 1.4
+john = 4
+eva = 1
+adam = 0
+total_apples = john + eva + adam
+print('Current apples personally:','Jonh ='+john,'Eva ='+eva,'Adam'+adam,sep='  ')
+print('Total:',total_apples)
+
+# Homework 1.5
+kilometers = 12.25
+miles = 7.38
+inst = 1.61
+print('Miles to kilo:',f'{miles*kilometers}')
+print('Kilo to miles:',f'{kilometers/miles}')
+
+# Homework 1.6
+# This program has been written DD/MM/YYYY.
+# This program count seconds in 3 hours.
+a = 3 # Number of hours.
+seconds = 3600 # Number of seconds in 1 hour.
+print("Seconds:",f'{a*seconds}')  # Printing the number of hours.
