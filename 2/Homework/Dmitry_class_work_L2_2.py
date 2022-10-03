db4defd4c101592387ee5f8dd34268ea55154491
Dmitry <krio.alfa@gmail.com>
2022-10-03 16:59:48 +0300

    lection added

---
+++ 2/Homework/Dmitry_class_work_L2_2.py
@@ -0,0 +1,31 @@
+
+# # Exponentiation(power), Multiplication, Division, Integer Division, Remainder(modulo).
+# print(2 ** 3) # Int.
+# print(2 ** 3.) # Float.
+# print(2. ** 3) # Float.
+# print(2. ** 3.) # Float.
+
+# print(2 * 3) # Int
+# print(2 * 3.) # Float
+# print(2. * 3) # Float
+# print(2. * 3.) # Float
+
+# print(6 / 3) # Float
+# print(6 / 3.) # Float
+# print(6. / 3) # Float
+# print(6. / 3.) # Float
+
+# print(6 // 3) # Int
+# print(6 // 3.) # Float
+# print(6. // 3) # Float
+# print(6. // 3.) # Float
+# print(6 // 4) # Int
+# print(6. // 4) # Float
+
+# print(14 % 4) # Int
+# print(14 % 4.) # Float
+# print(14. % 4) # Float
+# print(14. % 4.) # Float
+# # Remember not try to divide by zero.
+
+# print(keyword.kwlist) # We cant name variables and function this words...
