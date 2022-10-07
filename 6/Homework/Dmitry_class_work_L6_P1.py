49e0403563e6402a375c4aba4ef04f73c96ab3d0
Dmitry <krio.alfa@gmail.com>
2022-10-07 18:22:57 +0300

    Lection-6 added

---
+++ 6/Homework/Dmitry_class_work_L6_P1.py
@@ -0,0 +1,16 @@
+# school_class = {}
+
+# while True:  # Class journal.
+#     name = input("Name: ")
+#     if name == '':
+#         break
+#     score = int(input("Score: "))
+#     if score not in range(0,11):
+#         break
+#     if name in school_class:
+#         school_class[name] += (score,)  # Tuples and dictions can work together.
+#     else:
+#         school_class[name] + (score,)
+
+# tup = 4,  # Tuple...
+
